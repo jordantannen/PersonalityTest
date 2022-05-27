cb6d0ee376cd9088fbc99846ad4f724f72c827d6
Jordan <jordantannen@gmail.com>
2022-05-26 23:27:12 -0400

    Start setting up project

---
+++ PersonalityTest.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class PersonalityTest {
+    public static void main(String[] args) throws FileNotFoundException{
+
+        Scanner input = new Scanner(new File("personality.txt"));
+        PrintStream output = new PrintStream(new File("output.text"));
+
+        int tally = 0;
+
+        while (input.hasNext()){
+            if tally
+        }
+
+
+        while (input.hasNext()){
+            output.println(input.nextLine());
+        }
+
+    }
+    public static int calculateScores(String results){
+
+    }
+}
